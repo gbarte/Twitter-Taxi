328d53be8fc7d7a8f1902156e42e7ddf0de83700
Jordan Hale <jhale2@sheffield.ac.uk>
2019-03-05 15:38:45 +0000

    Includes a function to check if there are any new orders. -Needs to be connected to database. -Needs to loop to check for new tweets every few seconds

---
+++ twitter.rb
@@ -0,0 +1,43 @@
+require 'twitter'
+config = {
+:consumer_key => 'lqCpGOUMYGXaSLMAYUyqH9MhX',
+:consumer_secret => 'aMUTzyD8UlzcHqyWTtgrgNA6L7LKy9tL6WP7jCRCrVVxYzJa1D',
+:access_token => '1092447528761610240-sKtozZ8IDihSED4UuFK2fk39bWNCa7',
+:access_token_secret => 'CbNNmZHlIZJSttTk3OlAhZ4vdfE9BJtv0iBDYGy8YHar7'
+}
+client = Twitter::REST::Client.new(config)
+
+tweets = client.user_timeline('ise19team09')
+
+
+def checkIfNewTweets(newestOrder, tweets)
+    i = 1
+    newTweets = []
+    newestTweet = tweets.take(1)
+    #Checks if the newest tweet is the newest order
+    while (newestTweet != newestOrder)
+        #if not then there is a new order
+        #Gets next batch of new tweets
+        mostRecent = tweets.take(i)
+        #Checks newest out of grabbed tweets
+        if mostRecent[i-1] != newestOrder 
+            #add to new orders
+            newTweets << mostRecent[i-1]
+            i += 1
+        elsif mostRecent[i-1] == newestOrder
+            #is equal to the newest order so stop
+            break;
+        end
+    end
+    
+    return newestTweet, newTweets
+end
+
+test = tweets.take(5)
+newestOrder = test[4]
+
+newestOrder, newTweets = checkIfNewTweets(newestOrder, tweets)
+
+newTweets.each do |x|
+    puts x.id
+end
