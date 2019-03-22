b97bea8e16415eae36c216da7c58926529df08d3
Ziyang Lin <zlin19@sheffield.ac.uk>
2019-03-22 02:12:52 +0000

    Lacking order id in orderhistory is fixed

--- index.rb
+++ index.rb
@@ -178,7 +178,7 @@ end
 
 
 get '/orderHistory' do    
-    @results = @db.execute('SELECT order_id, pickup, destination, time, tier_id
+    @results = @db.execute('SELECT order_id, user_id, pickup, destination, time, tier_id
                             FROM OrderHistory WHERE user_id = ? ' ,[$userID])
     
     erb :orderHistory
